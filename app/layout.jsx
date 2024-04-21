c73860f8e3ab9969946bf2d025eed613212186c4
Raghav Sharma <raghu250407@gmail.com>
2024-04-21 15:54:33 +0530

    setting up

---
+++ app/layout.jsx
@@ -0,0 +1,16 @@
+import "./globals.css"
+
+export const metadata = {
+  title: "Raghav Sharma Portfolio",
+  description: "Raghav Sharma's Portfolio, Full stack Developer",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+     <body>
+      {children}
+     </body>
+    </html>
+  );
+}

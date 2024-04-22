a4fff8bd9c7de74a636986921c38475e087aaf0e
Raghav Sharma <raghu250407@gmail.com>
2024-04-23 00:00:17 +0530

    css tweaks

--- components/ProjectPageComponents/ProjectCard.jsx
+++ components/ProjectPageComponents/ProjectCard.jsx
@@ -9,29 +9,33 @@ const ProjectCard = ({ project }) => {
         alt={name}
         className="object-cover rounded-lg mb-4 w-1/3 mr-4"
       />
-      <div className="w-2/3">
-        <div className="text-2xl font-bold mb-2">{name}</div>
-        <div className="text-gray-600 mb-4">{description}</div>
-        <div className="text-sm text-gray-500 mb-2">
-          Tech Stack: {techStack}
+      <div className="w-2/3 flex flex-col justify-between p-2">
+        <div>
+          <div className="text-2xl font-bold mb-2">{name}</div>
+          <div className="text-gray-600 mb-4">{description}</div>
+          <div className="text-sm text-gray-500 mb-2">
+            Tech Stack: {techStack}
+          </div>
         </div>
-        <div className="flex justify-between">
-          <a
-            href={githubLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500 hover:underline"
-          >
-            GitHub
-          </a>
-          <a
-            href={deployedLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500 hover:underline"
-          >
-            Project Link
-          </a>
+        <div>
+          <div className="flex justify-between">
+            <a
+              href={githubLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline"
+            >
+              GitHub
+            </a>
+            <a
+              href={deployedLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline"
+            >
+              Project Link
+            </a>
+          </div>
         </div>
       </div>
     </div>

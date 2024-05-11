5a69555abd9c5328753f6738f339d2bacf38c0ce
Some1Uknow <raghu250407@gmail.com>
2024-05-12 00:18:00 +0530

    created blog page

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>blogs</div>
-  )
-}
+    <>
+      
+    </>
+  );
+};
 
-export default page
+export default page;

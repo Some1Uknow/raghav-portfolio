82daa95e7a0d0ecbb4a062268a94232decbcf01f
Some1Uknow <raghu250407@gmail.com>
2024-07-27 23:43:58 +0530

    fixed url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,9 +3,24 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+      url: `https://raghav-in.live`,
       lastModified: new Date(),
       priority: 1,
+    },
+    {
+      url: `https://raghav-in.live/about`,
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: `https://raghav-in.live/work`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: `https://raghav-in.live/contact`,
+      lastModified: new Date(),
+      priority: 0.7,
     },
   ]
 }
